2b0898378953c7cf2475775c8b456ccb37c7d92f
Amey8883 <139483839+Amey8883@users.noreply.github.com>
2024-08-07 15:25:07 +0530

    Create LaunchBrowser.java

---
+++ LaunchBrowser.java
@@ -0,0 +1,18 @@
+// Package declaration
+package BasicNa;
+
+// Importing necessary class from Selenium library
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Aim:
+ * This program is designed to automate the process of launching a Chrome web browser.
+ */
+public class LaunchBrowser 
+{
+    public static void main(String[] args)
+    {
+        // Open the Browser
+        ChromeDriver driver = new ChromeDriver();	
+    }
+}
