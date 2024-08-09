01fe099fad0c4d498f46d65cfa1705c5185199e9
Amey8883 <139483839+Amey8883@users.noreply.github.com>
2024-08-09 19:51:45 +0530

    Create ShoppingCartClassName.java

---
+++ Programs/ShoppingCartClassName.java
@@ -0,0 +1,50 @@
+package BasicNa;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/*
+ * Aim: This script automates the process of verifying the title of the
+ * Demo Web Shop homepage and clicking on the "Shopping Cart" link if the title matches.
+ */
+public class ShoppingCartClassName {
+
+    public static void main(String[] args) throws InterruptedException {
+        
+        // Expected title of the Demo Web Shop homepage
+        String expected_result = "Demo Web Shop";
+        
+        // Initialize ChromeDriver
+        ChromeDriver driver = new ChromeDriver();
+        
+        // Maximize the browser window
+        driver.manage().window().maximize();
+        
+        // Wait for 3 seconds
+        Thread.sleep(3000);
+        
+        // Navigate to the Demo Web Shop homepage
+        driver.get("https://demowebshop.tricentis.com/");
+        
+        // Get the title of the current page
+        String actual_result = driver.getTitle();
+        
+        // Wait for 3 seconds
+        Thread.sleep(3000);
+        
+        // Check if the actual title matches the expected title
+        if(actual_result.equals(expected_result))
+        {
+            System.out.println("I am in the DWS homepage");
+            // Find the "Shopping Cart" link by class name and click it
+            driver.findElement(By.className("cart-label")).click();
+        } else {
+            System.out.println("I am not in the DWS homepage");
+            // Quit the browser if the title does not match
+            driver.quit();
+        }
+        
+        // Close the browser
+        driver.close();
+    }
+}
